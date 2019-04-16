1e1870ead087391f798a2376b262056b32c959f2
brinda behal <brindab50@gmail.com>
2019-04-16 02:36:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 require "pry"
 def prime?(x)
-  prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+  if x < 2 
+    return false 
+  else 
   range = (2..x-1)
  # binding.pry
   range.to_a.all? do |prime_number| 
+    
      x % prime_number != 0
+     
   end 
+end
 end
