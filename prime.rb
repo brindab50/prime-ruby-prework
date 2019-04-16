0e065795c2bac84319c618db2bdb08b333d20248
brinda behal <brindab50@gmail.com>
2019-04-16 02:36:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
-require "pry"
+
 def prime?(x)
   if x < 2 
     return false 
   else 
   range = (2..x-1)
- # binding.pry
+ 
   range.to_a.all? do |prime_number| 
     
      x % prime_number != 0
