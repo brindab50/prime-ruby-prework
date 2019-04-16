3100a1db6f44e2012dabbcb013b5d9105ab47e11
brinda behal <brindab50@gmail.com>
2019-04-16 02:14:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(x)
+  prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+  
+  prime.each do |prime_number| 
+     if x == prime_number 
+       return true
+       
+     end
+  end 
+end
