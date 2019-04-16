40df816bcca97a6d5df42f3a9e8980669417fbdb
brinda behal <brindab50@gmail.com>
2019-04-16 02:28:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,8 @@ def prime?(x)
   prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
   range = (2..x-1)
   binding.pry
-  prime.each do |prime_number| 
-     if x == prime_number 
-       return true
-       
+  range.to_a.all? do |prime_number| 
+     x * 
      end
   end 
 end
